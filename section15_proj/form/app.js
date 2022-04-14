5ef73191357b9b20a69abbbbebf9ae5511341708
brian <bdkuhn602@gmail.com>
2022-04-14 16:57:04 -0700

    capture form data

--- section15_proj/form/app.js
+++ section15_proj/form/app.js
@@ -1,6 +1,12 @@
 const form = document.querySelector('#signup-form');
+const creditCardNum = document.querySelector('#cc');
+const termsAccept = document.querySelector('#terms');
+const veggieSel = document.querySelector('#veggie');
 
 form.addEventListener('submit', (e) => {
-    alert('FORM SUBMITTED');
     e.preventDefault();
+    alert('FORM SUBMITTED');
+    console.log('cc', creditCardNum.value);
+    console.log('terms', termsAccept.checked);
+    console.log('veggie', veggieSel.value);
 });
