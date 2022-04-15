d0ffa67bc70a0516309fff080f985f2fc10b0f2b
brian <bdkuhn602@gmail.com>
2022-04-14 17:11:35 -0700

    capture form data

--- section15_proj/form/app.js
+++ section15_proj/form/app.js
@@ -2,11 +2,12 @@ const form = document.querySelector('#signup-form');
 const creditCardNum = document.querySelector('#cc');
 const termsAccept = document.querySelector('#terms');
 const veggieSel = document.querySelector('#veggie');
+const formData = {};
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('FORM SUBMITTED');
-    console.log('cc', creditCardNum.value);
-    console.log('terms', termsAccept.checked);
-    console.log('veggie', veggieSel.value);
-});
+for (let input of [creditCardNum, termsAccept, veggieSel]) {
+    input.addEventListener('input', ({target}) => {
+        const {name, type, value, checked} = target;
+        formData[name] = type === 'checkbox' ? checked : value;
+        console.log(formData);
+    })
+}
