a98a838311ea85f71a2e25d11e2e07fefbbcdb70
brian <bdkuhn602@gmail.com>
2022-04-17 15:43:01 -0700

    finish xmlhttp demo

--- section18_proj/xmlhttp/app.js
+++ section18_proj/xmlhttp/app.js
@@ -1,16 +1,27 @@
 const firstReq = new XMLHttpRequest();
 
-firstReq.addEventListener('load', function() {
-    console.log("It worked!");
+firstReq.addEventListener('load', function () {
+    console.log('First request worked!');
     const data = JSON.parse(this.responseText);
-    for (let planet of data.results) {
-        console.log(planet.name);
-    }
+    const filmURL = data.results[0].films[0];
+    const filmReq = new XMLHttpRequest();
+    filmReq.addEventListener('load', function () {
+        console.log('Second request worked!');
+        console.log(JSON.parse(this.responseText));
+    });
+    filmReq.addEventListener('error', function () {
+        console.log('ERROR!', e);
+    });
+    filmReq.open('GET', filmURL);
+    filmReq.send();
+    // for (let planet of data.results) {
+    //     console.log(planet.name);
+    // }
 });
 firstReq.addEventListener('error', () => {
-    console.log("ERROR!");
+    console.log('ERROR!');
 });
-firstReq.open("GET", "https://swapi.dev/api/planets");
+firstReq.open('GET', 'https://swapi.dev/api/planets');
 firstReq.send();
 
-console.log("Request sent.");
+console.log('Request sent.');
