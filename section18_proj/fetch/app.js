d2b598414cde76fb592e349daf154e9c5a118289
brian <bdkuhn602@gmail.com>
2022-04-17 16:48:54 -0700

    chaining fetch requests

--- section18_proj/fetch/app.js
+++ section18_proj/fetch/app.js
@@ -2,13 +2,23 @@ fetch('https://swapi.dev/api/planets')
     .then((response) => {
         if (!response.ok) {
             throw new Error(`Status code error: ${response.status}`);
-        } else {
-            response.json().then((data) => {
-                for (let planet of data.results) {
-                    console.log(planet.name);
-                }
-            });
         }
+        return response.json();
+    })
+    .then((data) => {
+        console.log('fetched first 10 planets');
+        const filmURL = data.results[0].films[0];
+        return fetch(filmURL);
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`Status code error: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log('fetched first film of first planet');
+        console.log(data.title);
     })
     // fetch will only catch errors due to connection failure, not 404 or 500
     .catch((err) => {
