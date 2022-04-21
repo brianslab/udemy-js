50a4f25023695117169f17903f5227e45ebd3b3e
brian <bdkuhn602@gmail.com>
2022-04-20 21:28:10 -0700

    add basic dropdown menu on input

--- section21_proj/app.js
+++ section21_proj/app.js
@@ -6,13 +6,28 @@ const fetchData = async (searchTerm) => {
         }
     });
 
-    console.log(response.data);
+    if (response.data.Error) {
+        return [];
+    }
+
+    return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const onInput = (event) => {
-    fetchData(event.target.value);
+const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+
+    for (let movie of movies) {
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+            <h1>${movie.Title}</h1>    
+            <img src="${movie.Poster}"/>
+        `;
+
+        document.querySelector('#target').appendChild(div);
+    }
 };
 
 input.addEventListener('input', debounce(onInput, 500));
