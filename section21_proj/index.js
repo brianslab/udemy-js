46faf68864c9212015f12dd47433570712a5992c
brian <bdkuhn602@gmail.com>
2022-04-20 20:45:32 -0700

    add api call function

--- section21_proj/index.js
+++ section21_proj/index.js
@@ -1,1 +1,24 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com', {
+        params : {
+            apikey : 'e823d88e',
+            s      : searchTerm
+        }
+    });
+
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+let timeoutID;
+const onInput = (event) => {
+    if (timeoutID) {
+        clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 1000);
+};
+
+input.addEventListener('input', onInput);
