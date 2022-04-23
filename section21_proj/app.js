6d71f9592c34ab083bfc23e240b97eeaf1571fc5
Brian Kuhn <bdkuhn602@gmail.com>
2022-04-23 04:10:27 -0700

    print movie selection info

--- section21_proj/app.js
+++ section21_proj/app.js
@@ -47,6 +47,11 @@ const onInput = async (event) => {
             <img src="${imgSrc}"/>
             ${movie.Title}    
         `;
+        suggestion.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        });
 
         resultsWrapper.appendChild(suggestion);
     }
@@ -59,3 +64,33 @@ document.addEventListener('click', (event) => {
         dropdown.classList.remove('is-active');
     }
 });
+
+const onMovieSelect = async (movie) => {
+    const response = await axios.get('http://www.omdbapi.com', {
+        params : {
+            apikey : 'e823d88e',
+            i      : movie.imdbID
+        }
+    });
+
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}"/>
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+    `;
+};
