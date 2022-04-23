3570fae27c1824a01421601883e27229486470c0
Brian Kuhn <bdkuhn602@gmail.com>
2022-04-23 04:54:42 -0700

    begin autocomplete refactor. make function agnostic and separate into own file

--- section21_proj/app.js
+++ section21_proj/app.js
@@ -13,56 +13,14 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
-const finder = document.querySelector('.autocomplete');
-finder.innerHTML = `
-    <label><b>Search for a Movie</b></label>
-    <input class="input"/>
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (event) => {
-    const movies = await fetchData(event.target.value);
-
-    if (!movies.length) {
-        dropdown.classList.remove('is-active');
-        return;
-    }
-
-    resultsWrapper.innerHTML = '';
-    dropdown.classList.add('is-active');
-    for (let movie of movies) {
-        const suggestion = document.createElement('a');
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-        suggestion.classList.add('dropdown-item');
-        suggestion.innerHTML = `
-            <img src="${imgSrc}"/>
-            ${movie.Title}    
-        `;
-        suggestion.addEventListener('click', () => {
-            dropdown.classList.remove('is-active');
-            input.value = movie.Title;
-            onMovieSelect(movie);
-        });
-
-        resultsWrapper.appendChild(suggestion);
-    }
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', (event) => {
-    if (!finder.contains(event.target)) {
-        dropdown.classList.remove('is-active');
-    }
+createAutoComplete({
+    root : document.querySelector('.autocomplete')
+});
+createAutoComplete({
+    root : document.querySelector('.autocomplete-two')
+});
+createAutoComplete({
+    root : document.querySelector('.autocomplete-three')
 });
 
 const onMovieSelect = async (movie) => {
@@ -112,5 +70,5 @@ const movieTemplate = (movieDetail) => {
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
-            `;
+    `;
 };
