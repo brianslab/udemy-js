f879e02cc3feb9cd2ace4dbead068320f0d3ec30
brian <bdkuhn602@gmail.com>
2022-04-24 19:34:27 -0700

    finish canvas demo

--- section22_proj/app.js
+++ section22_proj/app.js
@@ -1,4 +1,15 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const {
+    Engine,
+    Render,
+    Runner,
+    World,
+    Bodies,
+    MouseConstraint,
+    Mouse
+} = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,18 +17,50 @@ const render = Render.create({
     element : document.body,
     engine  : engine,
     options : {
-        width  : 800,
-        height : 600
+        wireframes : false,
+        width,
+        height
     }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
+World.add(
+    world,
+    MouseConstraint.create(engine, {
+        mouse : Mouse.create(render.canvas)
+    })
+);
+
 // Walls
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
+
+// Random shapes
+for (let i = 0; i < 30; i++) {
+    if (Math.random() > 0.5) {
+        World.add(
+            world,
+            Bodies.rectangle(
+                Math.random() * width,
+                Math.random() * height,
+                50,
+                50
+            )
+        );
+    } else {
+        World.add(
+            world,
+            Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+                render : {
+                    fillStyle : 'green'
+                }
+            })
+        );
+    }
+}
