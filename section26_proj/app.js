87ea1a68e86db8562b0bdb0d3585f54f0f2eacbd
brian <bdkuhn602@gmail.com>
2022-05-02 01:35:23 -0700

    parse form data

--- section26_proj/app.js
+++ section26_proj/app.js
@@ -1,1 +1,35 @@
-console.log('hi from ' + process.cwd());
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email"/>
+            <input name="password" placeholder="password"/>
+            <input name="passwordConfirmation" placeholder="password confirmation"/>
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+    // get access to email, password, and passwordConfirmation
+    req.on('data', (data) => {
+        const parsed = data.toString('utf8').split('&');
+        const formData = {};
+
+        for (let pair of parsed) {
+            const [ key, value ] = pair.split('=');
+            formData[key] = value;
+        }
+        console.log(formData);
+    });
+    res.send('Account Created!');
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
