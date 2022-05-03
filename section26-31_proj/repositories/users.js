ee1f17c14317be3dd3271411e627a644ddcfd183
brian <bdkuhn602@gmail.com>
2022-05-03 05:01:56 -0700

    updade record method

--- section26-31_proj/repositories/users.js
+++ section26-31_proj/repositories/users.js
@@ -54,11 +54,24 @@ class UsersRepository {
         const filteredRecords = records.filter((record) => record.id !== id);
         await this.writeAll(filteredRecords);
     }
+
+    async update (id, attrs) {
+        const records = await this.getAll();
+        const record = records.find((record) => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found.`);
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.delete('055442e6');
+    // await repo.create({ email: 'me@example.com' });
+    await repo.update('123456', { password: 'mypass' });
 };
 test();
