5d635612fe3629b5916f424081d9f4a8fa7708bf
brian <bdkuhn602@gmail.com>
2022-05-03 05:28:27 -0700

    basic account creation error handling

--- section26-31_proj/app.js
+++ section26-31_proj/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const usersRepo = require('./repositories/users');
+const users = require('./repositories/users');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -17,8 +19,18 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+    const { email, password, passwordConfirmation } = req.body;
+
+    const existingUser = await usersRepo.getOneBy({ email });
+    if (existingUser) {
+        return res.send('Email in use.');
+    }
+
+    if (password !== passwordConfirmation) {
+        return res.send('Passwords must match.');
+    }
+
     res.send('Account Created!');
 });
 
