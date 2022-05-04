0e2e6580d57e8d7598a35a45e513bfca92b435ea
brian <bdkuhn602@gmail.com>
2022-05-03 23:46:59 -0700

    start implementing cookies

--- section26-31_proj/app.js
+++ section26-31_proj/app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const usersRepo = require('./repositories/users');
+const cookieSession = require('cookie-session');
 const users = require('./repositories/users');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    cookieSession({
+        keys : [ 'UKzTCg0mf8hEh9FQDpxdQRPUigDebudR' ]
+    })
+);
 
 app.get('/', (req, res) => {
     res.send(`
     <div>
+        Your ID is: ${req.session.userID}
         <form method="POST">
             <input name="email" placeholder="email"/>
             <input name="password" placeholder="password"/>
@@ -30,6 +37,12 @@ app.post('/', async (req, res) => {
     if (password !== passwordConfirmation) {
         return res.send('Passwords must match.');
     }
+
+    // create a user in users repo
+    const user = await usersRepo.create({ email, password });
+
+    // store the id inside the users cookie
+    req.session.userID = user.id; // added by cookie session
 
     res.send('Account Created!');
 });
