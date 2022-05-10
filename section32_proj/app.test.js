745c7e9da0087bbcdfbef13189bcffe17e763e2e
brian <bdkuhn602@gmail.com>
2022-05-10 04:14:39 -0700

    use test helper function for descriptions and independent testing

--- section32_proj/app.test.js
+++ section32_proj/app.test.js
@@ -1,25 +1,37 @@
 const { forEach, map } = require('./app');
 
-let sum = 0;
-forEach([ 1, 2, 3 ], (value) => {
-    sum += value;
+const test = (desc, fn) => {
+    console.log('***===*** TESTING:', desc, '***===***');
+    try {
+        fn();
+    } catch (err) {
+        console.log(err.message);
+    }
+};
+
+test('forEach', () => {
+    let sum = 0;
+    forEach([ 1, 2, 3 ], (value) => {
+        sum += value;
+    });
+
+    if (sum !== 6) {
+        throw new Error('Expected summing array to equal 6');
+    }
 });
 
-if (sum !== 6) {
-    throw new Error('Expected summing array to equal 6');
-}
+test('map', () => {
+    const result = map([ 1, 2, 3 ], (value) => {
+        return value * 2;
+    });
 
-// ==============
-const result = map([ 1, 2, 3 ], (value) => {
-    return value * 2;
+    if (result[0] !== 2) {
+        throw new Error(`Expected 2, but found ${result[0]}`);
+    }
+    if (result[1] !== 4) {
+        throw new Error(`Expected 2, but found ${result[1]}`);
+    }
+    if (result[2] !== 6) {
+        throw new Error(`Expected 2, but found ${result[2]}`);
+    }
 });
-
-if (result[0] !== 2) {
-    throw new Error(`Expected 2, but found ${result[0]}`);
-}
-if (result[1] !== 4) {
-    throw new Error(`Expected 2, but found ${result[1]}`);
-}
-if (result[2] !== 6) {
-    throw new Error(`Expected 2, but found ${result[2]}`);
-}
