297a5bc18a0022c2571cb8a4f83879f217b1f86d
brian <bdkuhn602@gmail.com>
2022-05-10 04:28:23 -0700

    refactor error messages with assert.strictEqual

--- section32_proj/app.test.js
+++ section32_proj/app.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const { forEach, map } = require('./app');
 
 const test = (desc, fn) => {
@@ -15,9 +16,7 @@ test('forEach', () => {
         sum += value;
     });
 
-    if (sum !== 6) {
-        throw new Error('Expected summing array to equal 6');
-    }
+    assert.strictEqual(sum, 6, 'sample custom message');
 });
 
 test('map', () => {
@@ -25,13 +24,7 @@ test('map', () => {
         return value * 2;
     });
 
-    if (result[0] !== 2) {
-        throw new Error(`Expected 2, but found ${result[0]}`);
-    }
-    if (result[1] !== 4) {
-        throw new Error(`Expected 2, but found ${result[1]}`);
-    }
-    if (result[2] !== 6) {
-        throw new Error(`Expected 2, but found ${result[2]}`);
-    }
+    assert.strictEqual(result[0], 2);
+    assert.strictEqual(result[1], 4);
+    assert.strictEqual(result[2], 6);
 });
