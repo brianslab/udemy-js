7af7fd105cfaf812416a93470680c38a3f8620a9
brian <bdkuhn602@gmail.com>
2022-05-11 04:13:39 -0700

    test for valid and invalid inputs

--- section33_proj/sample_web_proj/test/app.test.js
+++ section33_proj/sample_web_proj/test/app.test.js
@@ -20,5 +20,19 @@ it('shows a success message with valid email', async () => {
 
     const h1 = dom.window.document.querySelector('h1');
 
-    console.log('contents of h1:', h1.innerHTML);
+    assert.strictEqual(h1.innerHTML, 'Looks good!');
+});
+
+it('shows a failure message with invalid email', async () => {
+    const dom = await render('index.html');
+
+    const input = dom.window.document.querySelector('input');
+    input.value = 'test123!#$';
+    dom.window.document
+        .querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'));
+
+    const h1 = dom.window.document.querySelector('h1');
+
+    assert.strictEqual(h1.innerHTML, 'Invalid email');
 });
