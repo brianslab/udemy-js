096938aef46044952af387a8770461666570aa61
brian <bdkuhn602@gmail.com>
2022-05-11 03:49:38 -0700

    allow for async unit tests

--- section33_proj/runner.js
+++ section33_proj/runner.js
@@ -19,10 +19,10 @@ class Runner {
             global.beforeEach = (fn) => {
                 beforeEaches.push(fn);
             };
-            global.it = (desc, fn) => {
+            global.it = async (desc, fn) => {
                 beforeEaches.forEach((func) => func());
                 try {
-                    fn();
+                    await fn();
                     console.log(chalk.green(`\t${desc} - OK`));
                 } catch (err) {
                     const message = err.message.replace(/\n/g, '\n\t\t');
